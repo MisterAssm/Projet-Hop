462d788d30b33634bb7e6522545d7a92c63b74c9
anaishdc <hidouche.ouarda@gmail.com>
2024-11-25 20:21:56 +0100

    constructeur Field

--- src/Field.java
+++ src/Field.java
@@ -1,30 +1,35 @@
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 public class Field {
 
 
 
-    public static final int ALTITUDE_GAP = 80; // ecart d'altitude entre deux blocs
-    public static final int START_ALTITUDE = 40; :// altitude de premier bloc
+    public static final int ALTITUDE_GAP = 80; //ecart d'altitude entre deux blocs
+    public static final int START_ALTITUDE = 40; //altitude de premier bloc
 
-    public final int width, height; // largeur et hauteur de terrain 
+    public final int width;
+    public final int height;  //largeur et hauteur de terrain 
 
-    private int bottom, top; // bottom and top altitude
+    private int bottom, top;  //bottom and top altitude
 
-    private final ArrayList<Block> blocks ;
+    private final ArrayDeque<Block> blocks ;
     private int level=0;
+    private static final int maxLevel=6;
+
+
+
     public Field(int width, int height) {
         this.width = width; 
         this.height = height;
         this.top=height;
-        this.blocks=new ArrayList <>();
+        this.blocks =new ArrayDeque<>();
 
         // Ajout du premier block 
-        blocks.add(new Block(this.width-100,40,100));
+        this.blocks.addFirst(new Block(this.width-100,40,100));
 
         // deuxieme bloc a partir d une altitude de 40+80
-        for(int altitude=120, altitude<this.height;altitude+=80){
-            blocks.add(Block.blockAleatoire(this.level, altitude,this.width));
+        for(int altitude=120; altitude<this.height;altitude+=80){
+            this.blocks.addFirst(Block.blockAleatoire(this.level, altitude,this.width));
         }
     }
 
