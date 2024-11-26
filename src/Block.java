11c26d4ba91cce419dc1c2b19fb7e673800502c8
Assim Zemouchi <assim.zpr@gmail.com>
2024-11-26 14:31:50 +0100

    Add initial blocks

--- src/Block.java
+++ src/Block.java
@@ -2,13 +2,13 @@ import java.util.Random;
 
 public class Block {
 
+    public static final int INITIAL_MIN_WIDTH = 50;
+    public static final int INITIAL_MAX_WIDTH = 100;
 
     private final int x, y;
     private final int width;
 
-    private final int minWidth=50;
-    private final int maxWidth=100;
-
+    private static final Random random = new Random();
 
     public Block(int x, int y, int width) {
         this.x = x; // la position horizontal 
@@ -16,34 +16,31 @@ public class Block {
         this.width = width; // la largeur du bloc  
     }
 
-    // des block de largeur aleatoire 
-
-    // generer un nombre aleatoire dans un intervalle donneé 
-    public static int nombreAleatoire  ( int min, int max){
-            Random random = new Random();
-            return min + random.nextInt(max-min);
+    // générer un nombre aléatoire dans un intervalle (inclus)
+    public static int randomNumber(int min, int max) {
+        return min + random.nextInt(max - min);
     }
 
-    public static Block blockAleatoire (int niveau,int y,int largeurField){
-        int width= Block.nombreAleatoire(50-niveau*5,100-niveau*10);
-        int x= Block.nombreAleatoire(0,largeurField-width); // pour ne pas depasser la zone
-        return new Block (x,y,width);
+    public static Block randomBlock(int niveau, int y, int largeurField) {
+        int width = Block.randomNumber(50 - niveau * 5, 100 - niveau * 10); // valeurs dépendant du niveau
+        int x = Block.randomNumber(0, largeurField - width); // pour ne pas depasser la zone
+        return new Block(x, y, width);
     }
 
-    public int getWidth (){
+    public int getWidth() {
         return this.width;
     }
 
-    public int getAltitude (){
+    public int getAltitude() {
         return this.y;
     }
 
-    public  int getPosGauche(){
+    public int getPosGauche() {
         return this.x;
-    } 
+    }
 
-    public int getPosDroite(){
-        return this.x+ this.width;
+    public int getPosDroite() {
+        return this.x + this.width;
     }
 
 }
--- src/Field.java
+++ src/Field.java
@@ -2,36 +2,37 @@ import java.util.ArrayDeque;
 
 public class Field {
 
-
-
     public static final int ALTITUDE_GAP = 80; // ecart d'altitude entre deux blocs
     public static final int START_ALTITUDE = 40; // altitude de premier bloc
 
-    public final int width;
-    public final int height;  // largeur et hauteur de terrain
+    public static final int[] levelAltitude = new int[]{80, 800, 2000, 3200, 4800, 7200};
+    public static final int maxLevel = levelAltitude.length;
 
+    public final int width, height; // largeur et hauteur de terrain
     private int bottom, top;  // bottom and top altitude
-
-    private final ArrayDeque<Block> blocks ;
-    private int level=0;
-    private static final int maxLevel=6;
-
-
+    private int currentLevel = 0;
+    private final ArrayDeque<Block> blocks; // permet d'agir comme une file d'attente
 
     public Field(int width, int height) {
-        this.width = width; 
+        this.width = width;
         this.height = height;
-        this.top=height;
-        this.blocks =new ArrayDeque<>();
+        this.top = height;
+        this.bottom = 0;
+        this.blocks = new ArrayDeque<>();
 
         // Ajout du premier block 
-        this.blocks.addFirst(new Block(this.width-100,40,100));
+        this.blocks.addFirst(new Block(this.width - Block.INITIAL_MAX_WIDTH, START_ALTITUDE, Block.INITIAL_MAX_WIDTH));
 
         // deuxieme bloc a partir d une altitude de 40+80
-        for(int altitude=120; altitude<this.height;altitude+=80){
-            this.blocks.addFirst(Block.blockAleatoire(this.level, altitude,this.width));
+        for (int altitude = START_ALTITUDE + ALTITUDE_GAP; altitude < this.height; altitude += ALTITUDE_GAP) {
+            this.blocks.addFirst(Block.randomBlock(this.currentLevel, altitude, this.width));
+            altitude += ALTITUDE_GAP;
         }
     }
 
-    public void update() { }
+    public void updateCurrentLevel(int altitude) {
+        if (currentLevel < maxLevel && altitude > levelAltitude[currentLevel]) {
+            currentLevel++; // TODO: Faire une animation ?
+        }
+    }
 }
